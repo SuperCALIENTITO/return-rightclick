9b9f7d51a28d2f4e16f1e30384ae3bfefb9e9bf5
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-03-15 23:07:02 -0300

    Initial Release

---
+++ Enable Context Menu Wattpad.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Enable Context Menu Wattpad
+// @version      0.1
+// @namespace     https://gist.github.com/SuperCALIENTITO/2cfc5117eaaad5200ffd25b451ad5d38
+// @description  Trae devuelta el menu de contexto a la pagina Wattpad
+// @author       Vicentefelipechile
+// @author       https://stackoverflow.com/users/1727984/user1727984
+// @icon         https://i.imgur.com/bbykxTF.png
+// @downloadURL  https://gist.githubusercontent.com/SuperCALIENTITO/2cfc5117eaaad5200ffd25b451ad5d38/raw/f1b648827abfce7f92a9ffae8ebf53289e694acc/reenableRightClick.js
+// @updateURL    https://gist.githubusercontent.com/SuperCALIENTITO/2cfc5117eaaad5200ffd25b451ad5d38/raw/f1b648827abfce7f92a9ffae8ebf53289e694acc/reenableRightClick.js
+// @match        *.wattpad.com/*
+// @grant        none
+// ==/UserScript==
+
+// Codigo creado originalmente por https://stackoverflow.com/users/1727984/user1727984
+// Implementación por Vicentefelipechile
+(function(w){
+    var arr = ['contextmenu','copy','cut','paste','mousedown','mouseup','beforeunload','beforeprint'];
+    for(var i = 0, x; x = arr[i]; i++){
+        if(w['on' + x])w['on' + x] = null;
+        w.addEventListener(x, function(e){e.stopPropagation()}, true);
+    };
+    for(var j = 0, f; f = w.frames[j]; j++){try{w.callee(f)}catch(e){}}})(window);
