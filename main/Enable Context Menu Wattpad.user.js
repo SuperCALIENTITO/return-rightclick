049afb6235e4d769fe0d32ec201c6e5ce70c7810
SuperCALIENTITO <70909800+SuperCALIENTITO@users.noreply.github.com>
2022-03-16 22:46:23 -0300

    Update Enable Context Menu Wattpad.user.js

--- main/Enable Context Menu Wattpad.user.js
+++ main/Enable Context Menu Wattpad.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Enable Context Menu Wattpad
-// @version      0.1
+// @version      0.2
 // @namespace    https://github.com/SuperCALIENTITO/return-rightclick
 // @homespace    https://github.com/SuperCALIENTITO/return-rightclick
 // @description  Trae devuelta el menu de contexto a la pagina Wattpad
